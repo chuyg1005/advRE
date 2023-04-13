13c6f09aaeb71fd4a92f046a6958497d5761f381
chuyg <chuyg@smail.nju.edu.cn>
2023-04-13 14:38:03 +0000

    追踪genpt的rela2id.json文件

--- unist/scripts/eval_tacred.sh
+++ unist/scripts/eval_tacred.sh
@@ -1,7 +1,14 @@
 export CUDA_VISIBLE_DEVICES=0
-type=baseline
-dataset=tacred
-# split=test
-for split in 'test' 'test_two_new_entity' 'test_one_new_entity' 'test_new_subj_entity' 'test_new_obj_entity' 'test_two_old_entity' 'test_two_old_entity_new_pair' 'test_two_old_entity_old_pair' 'test_two_old_entity_old_pair_new_rela' 'test_two_old_entity_old_pair_old_rela'; do
-    python run.py --data_dir ../../data/re-datasets/${dataset} --train_tasks ${dataset} --eval_tasks ${dataset} --do_eval --eval_dir ./output/${dataset}-${type}/roberta-base/checkpoint-90 --eval_name ${split} --eval_only
-done
+type=ours
+# dataset=retacred
+dataset=retacred
+split=test
+# for split in 'test' 'test_two_new_entity' 'test_one_new_entity' 'test_new_subj_entity' 'test_new_obj_entity' 'test_two_old_entity' 'test_two_old_entity_new_pair' 'test_two_old_entity_old_pair' 'test_two_old_entity_old_pair_new_rela' 'test_two_old_entity_old_pair_old_rela'; do
+# python run.py --data_dir ../../data/re-datasets/${dataset} --train_tasks ${dataset} --eval_tasks ${dataset} --do_eval --eval_dir ./output/${dataset}-${type}/roberta-base/checkpoint-90 --eval_name ${split} --eval_only
+python run.py --data_dir ../../data/re-datasets/${dataset} --train_tasks ${dataset} --eval_tasks ${dataset} --do_eval --eval_dir ./output/${dataset}-${type}/roberta-base --eval_name ${split} --eval_only
+# done
+
+# for split in 'test_rev' 'test_rev_two_new_entity' 'test_rev_one_new_entity' 'test_rev_new_subj_entity' 'test_rev_new_obj_entity' 'test_rev_two_old_entity' 'test_rev_two_old_entity_new_pair' 'test_rev_two_old_entity_old_pair' 'test_rev_two_old_entity_old_pair_new_rela' 'test_rev_two_old_entity_old_pair_old_rela'; do
+#     python run.py --data_dir ../../data/re-datasets/${dataset} --train_tasks ${dataset} --eval_tasks ${dataset} --do_eval --eval_dir ./output/${dataset}-${type}/roberta-base/checkpoint-90 --eval_name ${split} --eval_only
+#     # python run.py --data_dir ../../data/re-datasets/${dataset} --train_tasks ${dataset} --eval_tasks ${dataset} --do_eval --eval_dir ./output/${dataset}-${type}/roberta-base --eval_name ${split} --eval_only
+# done
