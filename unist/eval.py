b54539649fc158d30ae175744de32acc262e132e
chuyg <chuyg@smail.nju.edu.cn>
2023-04-28 03:25:06 +0000

    完成了unist的eval.py评价文件

---
+++ unist/eval.py
@@ -0,0 +1,92 @@
+import os
+from argparse import ArgumentParser
+
+import torch
+from eval_metric import tacred_f1, tacred_mi_ma_f1
+from model import UniSTModel
+from torch.utils.data import DataLoader, SequentialSampler
+from tqdm import tqdm
+from transformers import AutoTokenizer
+
+from data import RETACREDDataset, TACREDDataset
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='../../re-datasets')
+    parser.add_argument('--ckpt_dir', type=str, required=True)
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--split', type=str, required=True)
+    parser.add_argument('--mask', action='store_true')
+
+    args = parser.parse_args()
+    # 读取参数
+    ckpt_dir = args.ckpt_dir
+    data_dir = args.data_dir
+    dataset = args.dataset
+    split = args.split
+    data_path = os.path.join(data_dir, dataset, 'splits', split+'.json')
+    no_task_desc = False
+    mask_entity = args.mask
+    model_name_or_path = 'roberta-base'
+    eval_batch_size = 64
+    max_sent_length=160
+    max_label_length=20
+
+    # 加载模型
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = UniSTModel.from_pretrained(ckpt_dir)
+    model.to(device)
+    tokenizer = AutoTokenizer.from_pretrained(ckpt_dir)
+
+    # 加载数据集
+    if dataset == 'tacred':
+        Dataset =TACREDDataset
+    elif dataset == 'retacred':
+        Dataset = RETACREDDataset
+    else:
+        assert 0, f'no such dataset: {dataset}.'
+    eval_dataset = Dataset(data_path, no_task_desc=no_task_desc, mode='test', mask_entity=mask_entity, mask_token=tokenizer.mask_token) 
+
+    # 评价模型
+    eval_sampler = SequentialSampler(eval_dataset)
+    eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=eval_batch_size, collate_fn=lambda x: list(map(list, zip(*x))))
+    
+    dists = []
+    labels = []
+    model.eval()
+
+    with torch.no_grad():
+        labelset = eval_dataset.labelset
+        labelset_inputs = tokenizer(labelset, padding=True, truncation=True, max_length=max_label_length, return_tensors="pt").to(device)
+        labelset_embeddings = model.embed(**labelset_inputs)
+    
+    for sent, pos, *other in tqdm(eval_dataloader, desc="Evaluating"):
+        labels.extend(pos)
+        sent_inputs = tokenizer(sent, padding=True, truncation=True, max_length=max_sent_length, return_tensors="pt", is_split_into_words=True).to(device)
+                         
+        with torch.no_grad():            
+            sent_embeddings = model.embed(**sent_inputs)
+            for i in range(len(sent_embeddings)):
+                sent_embedding = sent_embeddings[i].expand(labelset_embeddings.shape)
+                dist = model.dist_fn(sent_embedding, labelset_embeddings).detach().cpu().numpy().tolist()
+                dists.append(dist)  
+
+    preds = []
+    for i, dist in enumerate(dists):
+        pred = min(labelset, key=lambda x:dist[labelset.index(x)])
+        preds.append(pred)
+
+    assert len(preds) == len(eval_dataset)
+    # p, r, f1 = tacred_f1(labels, preds)
+    # 计算micro_f1和macro_f1
+    mi_f1, ma_f1 = tacred_mi_ma_f1(labels, preds, len(eval_dataset.labelset))
+
+    # 打印结果
+    # results = {
+        # "Micro precision": p,
+        # "Micro recall": r,
+        # "Micro f1": f1
+    # }
+
+    print(split)
+    print(f'mi_f1: {mi_f1*100:.2f}, ma_f1: {ma_f1*100:.2f}.')
--- unist/eval_metric.py
+++ unist/eval_metric.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 # ufet metric, mostly Borrowed from https://github.com/uwnlp/open_type
 def f1(p, r):
     if r == 0.:
@@ -79,3 +82,71 @@ def tacred_f1(labels, preds):
     if prec_micro + recall_micro > 0.0:
         f1_micro = 2.0 * prec_micro * recall_micro / (prec_micro + recall_micro)
     return prec_micro, recall_micro, f1_micro
+
+def tacred_mi_ma_f1(label, output, rel_num):
+    # 转换为字典
+    # print(label)
+    # print(output)
+
+    relas = set(label + output) # 求出所有label
+    rela2id = {rela:i for i, rela in enumerate(relas)}
+    label = [rela2id[l] for l in label]
+    output = [rela2id[l] for l in output]
+    na_num = rela2id['no relation']
+    
+    correct_by_relation = Counter()
+    guess_by_relation = Counter()
+    gold_by_relation = Counter()
+
+    for i in range(len(output)):
+        guess = output[i]
+        gold = label[i]
+
+        if guess == na_num:
+            guess = 0
+        elif guess < na_num:
+            guess += 1
+
+        if gold == na_num:
+            gold = 0
+        elif gold < na_num:
+            gold += 1
+
+        if gold == 0 and guess == 0:
+            continue
+        if gold == 0 and guess != 0:
+            guess_by_relation[guess] += 1
+        if gold != 0 and guess == 0:
+            gold_by_relation[gold] += 1
+        if gold != 0 and guess != 0:
+            guess_by_relation[guess] += 1
+            gold_by_relation[gold] += 1
+            if gold == guess:
+                correct_by_relation[gold] += 1
+
+    f1_by_relation = Counter()
+    recall_by_relation = Counter()
+    prec_by_relation = Counter()
+    for i in range(1, rel_num):
+        recall = 0
+        if gold_by_relation[i] > 0:
+            recall = correct_by_relation[i] / gold_by_relation[i]
+        precision = 0
+        if guess_by_relation[i] > 0:
+            precision = correct_by_relation[i] / guess_by_relation[i]
+        if recall + precision > 0:
+            f1_by_relation[i] = 2 * recall * precision / (recall + precision)
+        else:
+            f1_by_relation[i] = 0.
+        recall_by_relation[i] = recall
+        prec_by_relation[i] = precision
+
+    micro_f1 = 0
+    if sum(guess_by_relation.values()) != 0 and sum(correct_by_relation.values()) != 0:
+        recall = sum(correct_by_relation.values()) / sum(gold_by_relation.values())
+        prec = sum(correct_by_relation.values()) / sum(guess_by_relation.values())
+        micro_f1 = 2 * recall * prec / (recall + prec)
+
+    macro_f1 = sum(f1_by_relation.values()) / len(f1_by_relation)
+
+    return micro_f1, macro_f1
